0dad0787dc01b0cdeacf655b4484a36d800113f3
KsJjjjj <kjszywala@protonmail.com>
2022-11-04 20:03:11 +0000

    Toolbar overflow button done

--- WarehouseOperative/Views/MainWindow.xaml.cs
+++ WarehouseOperative/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -64,6 +65,33 @@ namespace WarehouseOperative.Views
                 {
                     Buttons_column.Width = new GridLength(150);
                 }
+            }
+        }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as Grid;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Background = Brushes.Transparent;
+                if (overflowGrid.IsMouseOver == true)
+                {
+                    overflowGrid.Background = Brushes.Transparent;
+                }
+            }
+
+            var overflowButton = toolBar.Template.FindName("OverflowButton", toolBar) as ToggleButton;
+            if (overflowButton != null)
+            {
+                overflowButton.Background = Brushes.Transparent;
+
+            }
+
+            var overflowPanel = toolBar.Template.FindName("PART_ToolBarOverflowPanel", toolBar) as ToolBarOverflowPanel;
+            if (overflowPanel != null)
+            {
+                overflowPanel.Background = Brushes.Gray;
             }
         }
     }
