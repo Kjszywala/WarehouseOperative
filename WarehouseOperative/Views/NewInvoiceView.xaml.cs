8171b52251e78b4d7951ae52a041c7e1bf439714
KsJjjjj <kjszywala@protonmail.com>
2022-11-07 17:29:42 +0000

    Bookmarks toolbar to do

--- WarehouseOperative/Views/NewInvoiceView.xaml.cs
+++ WarehouseOperative/Views/NewInvoiceView.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace WarehouseOperative.Views
+﻿namespace WarehouseOperative.Views
 {
     /// <summary>
     /// Interaction logic for NewInvoiceView.xaml
     /// </summary>
-    public partial class NewInvoiceView : AllViewBase
+    public partial class NewInvoiceView : AddRowViewBase
     {
         public NewInvoiceView()
         {
