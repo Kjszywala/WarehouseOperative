6261818dafd267bfa294f56be54d95f49c8fdba7
KsJjjjj <kjszywala@protonmail.com>
2022-12-21 20:47:30 +0000

    AllProductsViewModel done, adding to do

---
+++ WarehouseOperative/Models/EntitiesForView/ProductsForAllView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseOperative.Models.EntitiesForView
+{
+    public class ProductsForAllView
+    {
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public string CategoryCategoryDescription { get; set; }
+        public string SupplierCompanyName { get; set; }
+        public string ProductLogsDescription { get; set; }
+        public decimal ActualQuantity { get; set; }
+        public decimal ActualPrice { get; set; }
+        public string QuantityTypesQuantityType { get; set; }
+    }
+}
--- WarehouseOperative/ViewModels/AllViewModel/AllProductsViewModel.cs
+++ WarehouseOperative/ViewModels/AllViewModel/AllProductsViewModel.cs
@@ -1,12 +1,45 @@
-﻿namespace WarehouseOperative.ViewModels.AllViewModel
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using WarehouseOperative.Models.EntitiesForView;
+using WarehouseOperative.ViewModels.Abstract;
+
+namespace WarehouseOperative.ViewModels.AllViewModel
 {
-    public class AllProductsViewModel : WorkspaceViewModel
+    public class AllProductsViewModel : AllViewModel<ProductsForAllView>
     {
         #region Constructor
         public AllProductsViewModel()
+            : base("Products")
         {
-            //set the bookmark name here
-            DisplayName = "Products";
+        }
+        #endregion
+        #region Helpers
+        public override void Load()
+        {
+            try
+            {
+                List = new ObservableCollection<ProductsForAllView>(
+                    from products in WarehouseEntities.Products
+                    where products.IsActive == true
+                    select new ProductsForAllView
+                    {
+                        ProductName = products.ProductName,
+                        ProductCode = products.ProductCode,
+                        CategoryCategoryDescription = products.Categories.CategoryDescription,
+                        SupplierCompanyName = products.Suppliers.CompanyName,
+                        ProductLogsDescription = products.ProductLogs.ProductLogDescrition,
+                        ActualQuantity = products.ActualQuantity,
+                        ActualPrice = products.ActualPrice,
+                        QuantityTypesQuantityType = products.QuantityTypes.title
+                    }
+                );
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
         #endregion
     }
